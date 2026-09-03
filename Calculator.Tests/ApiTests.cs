f1dd595dda141515870ea6013dea0c36b71dfed8
TA0426 <ta0426.kh@gmail.com>
2026-09-03 18:44:04 +0200

    PostFight_ReturnsOk oppdatert gir success

--- Calculator.Tests/ApiTests.cs
+++ Calculator.Tests/ApiTests.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net.Http.Json;
+using OppgaveUkeEnModul3.Core;
 
 
 namespace Calculator.Tests;
@@ -46,8 +47,47 @@ public class ApiTests
     [Fact]
     public async Task PostFight_ReturnsOk()
     {
-        // Opprett testdata
-        // Send POST /Fight
-        // Forvent 200 OK
+        await using var factory =
+            new WebApplicationFactory<Program>();
+
+        using var client = factory.CreateClient();
+
+        var characterResponse = await client.PostAsJsonAsync(
+            "/StoreCharacters",
+            new
+            {
+                name = "Anna"
+            });
+
+        var character =
+            await characterResponse.Content.ReadFromJsonAsync<StoreCharacter>();
+
+        var monsterResponse = await client.PostAsJsonAsync(
+            "/StoreMonsters",
+            new
+            {
+                name = "Weak Monster",
+                quantity = 1,
+                typeOfMonster = "Goblin",
+                hp = 10,
+                damage = 1,
+                xpReward = 10,
+                description = "A weak test monster"
+            });
+
+        var monster =
+            await monsterResponse.Content.ReadFromJsonAsync<StoreMonster>();
+
+        var fightResponse = await client.PostAsJsonAsync(
+            "/Fight",
+            new
+            {
+                characterId = character!.Id,
+                monsterId = monster!.Id
+            });
+
+        Assert.Equal(
+            HttpStatusCode.OK,
+            fightResponse.StatusCode);
     }
 }
